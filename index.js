bb32d4529be12e1f5bc36cef29ce5e38c3a451f8
Emeka-Okpaleke <olisemekaokpaleke@yahoo.com>
2024-06-10 14:55:48 +0100

    Event listner if statment replaced toggle

--- index.js
+++ index.js
@@ -2,11 +2,7 @@ const mobileNav = document.querySelector(".mobile-hamburger-container");
 const hamburger = document.querySelector(".hamburger");
 
 hamburger.addEventListener('click', function(){
-    if(mobileNav.style.display === "none"){
-        mobileNav.style.display = "block";
-    }else{
-        mobileNav.style.display = "none";
-    }
+    mobileNav.classList.toggle('active')
 });
 
 
